5b90ab49470be1fb6d799277ae0aaf5209a68212
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2025-06-24 14:49:11 +0700

    update .env, axiosConfig.ts and userApi.ts

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -4,6 +4,7 @@ const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: apiUrl,
+  url: apiUrl,
   withCredentials: true, // Bật để gửi credentials (cookies, authorization headers)
   headers: {
     'Content-Type': 'application/json',
